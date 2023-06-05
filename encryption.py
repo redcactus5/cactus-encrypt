5e428af265f70ff13560304372bcd9213641fe19
redcactus5 <robocat899@gmail.com>
2023-06-05 11:21:52 -0700

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -50,115 +50,34 @@ def getNumberRange(start,end):
 
 
 def generateKey():
-    key=""
-    #loop to creat the rotor wirings
-    for rotor in range(ROTORNUM):
-        keyPeice=""
-        numberRange=getNumberRange(0,len(CHARACTERS))
-        #create a string of scrambled numbers to act as the rotor wiring
-        for i in range(len(numberRange)):
-            keyPeice+=str(numberRange.pop(randint(0,len(numberRange)-1)))
-            #add a comma as a seperator unless you are on the last number
-            if(len(numberRange)>=1):
-                keyPeice+=", "
-        #add a seperator between the keys and add the rotor wiring to the key
-        key+=keyPeice+" | "
-    #create and add the rotor start positions to the key with seperators
-    for i in range(ROTORNUM):
-        key+=(str(randint(0,len(CHARACTERS)))+" | ")
-    
-        
-    
-    #generate the initial scrambler wiring (near identical to  rotor wiring generation)
-    numberRange=getNumberRange(0,len(CHARACTERS))
-    initScrambler=""
-    for i in range(len(numberRange)):
-        initScrambler+=str(numberRange.pop(randint(0,len(numberRange)-1)))
-        #add a comma as a seperator unless you are on the last number
-        if(len(numberRange)>=1):
-            initScrambler+=", "
-    #add initial scrambler to the key
-    key+=initScrambler
-    print(key)
-    return key
+    #key structure [rotor keys],[rotor starts],[initial and final static cyphers]
+    pass
     
 
 
     
-def loadKey(key):
-    #first seperation of the text into lists
-    splitKeyStringList=key.rsplit(" | ")
-    key=[]
-    
-    #for all the rotor wirings
-    for i in range(ROTORNUM):
-        #pop the rotor wiring from splitkeystringlist
-        temp=splitKeyStringList.pop(0)
-        #split the wiring into a list of strings
-        temp=temp.rsplit(", ")
-        #convert the number strings in the wiring list into integers
-        for i in range(len(temp)):
-            temp[i]=int(temp[i])
-        #add the decoded wiring to key
-        key.append(temp)
-    
-    #for all rotor start postions
-    for i in range(ROTORNUM):
-        #covert the start position string into an integer and move it to key
-        key.append(int(splitKeyStringList.pop(0))) 
+def loadKey(inputKey):
+    pass
 
-
-
-    #decode the initial scrambler in the same way as the rotors
-    initialScrambler=splitKeyStringList.pop(0)
-    initialScrambler=initialScrambler.rsplit(", ")
-    for i in range(len(initialScrambler)):
-        initialScrambler[i]=int(initialScrambler[i])
-    #add the decoded scrambler key to key
-    key.append(temp)
-    
-    global loadedKey
-    loadedKey=key
-    return key
-    
-test=generateKey()    
-loadKey(test)
-
-
-def exportKey():
-    #get the currently loaded key
-    global loadedKey
-    #check to see if a key is loaded
-    if(loadedKey!=None):
-        
-        #if a key is loaded
-        key=""
-        #for all the rotor keys
-        for rotor in range(ROTORNUM):
-            keyPeice=""
-            #compile the key into a string
-            for i in range(len(loadedKey[rotor])):
-                keyPeice+=str(loadedKey[rotor][i])
-                if(i<len(loadedKey[rotor])-1):
-                    keyPeice+=", "
-            #add string to key string
-            key+=keyPeice+" | "
-        #convert start positions into strings and add them to key
-        for i in range(ROTORNUM):
-            key+=(str(loadedKey[i+ROTORNUM])+" | ")
-        
-        
-        print(key)
-
-        return key
-    return False
-        
     
 
 
-exportKey()
 
-print(test==exportKey())
+def exportKey(inputKey):
+    
+    pass
+    
+    
+
+  
+    
+
+
+    
+#something in either loading or exporting is breaking the initial scrambler key
+
+
+
 
 #to decrypt just trace the path back in reverse
     
