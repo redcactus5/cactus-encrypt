d9a1cbfb9dd7bd6afd19e767a127f2fb3b7b6d43
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2023-12-31 19:12:40 -0500

    Update cactus_encrpyt.py

--- source_code/cactus_encrpyt.py
+++ source_code/cactus_encrpyt.py
@@ -1,4 +1,8 @@
 import command_line_interface
 
-
-command_line_interface.start()
+try:
+    command_line_interface.start()
+except:
+    print("\n")*50
+    print("a serious error occured. the program has been closed for safety reasons.")
+    input("press enter to continue")
