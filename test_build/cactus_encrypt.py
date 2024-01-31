a0efb360d481a2f3843e65a47e6b0fdf9bc5389f
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2024-01-31 15:47:43 -0500

    fix bugs, add saftey check, finally write help file, (still needs proofreading)

---
+++ test_build/cactus_encrypt.py
@@ -0,0 +1,8 @@
+import command_line_interface
+
+#try:
+command_line_interface.start()
+#except:
+#    print("\n")*50
+#    print("a serious error occured. the program has quit to prevent damage")
+#    input("press enter to finish")
