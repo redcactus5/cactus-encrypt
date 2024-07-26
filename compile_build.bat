d88a71a3aef72acace6530cf4c4b9c7422cbed79
redcactus5 <135550649+redcactus5@users.noreply.github.com>
2024-07-25 20:02:28 -0700

    add a compilation automation script

---
+++ compile_build.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+echo searching for builds to compile...
+:: Check for the existence of Folder1
+if exist "release build" (
+    echo release build found!
+    echo starting compilation...
+    cd "release build"
+    python -m nuitka --standalone --windows-icon-from-ico=cactus_encrypt_icon.ico  cactus_encrypt.py
+    cd ..
+) else (
+    echo could not find release build.
+)
+
+:: Check for the existence of Folder2
+if exist "debug build" (
+    echo debug build found!
+    echo starting compilation...
+    cd "debug build"
+    python -m nuitka --standalone --windows-icon-from-ico=cactus_encrypt_icon.ico  cactus_encrypt.py
+    cd ..
+) else (
+    echo could not find debug build.
+)
+
+endlocal
